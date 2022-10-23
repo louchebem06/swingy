aecfe3a3b44d41295859bf9997317d136692e086
Ledda Bryan <louchebem06@gmail.com>
2022-10-24 00:21:10 +0200

    UPDATE: simulator

--- src/main/java/com/school42/swingy/Simulator.java
+++ src/main/java/com/school42/swingy/Simulator.java
@@ -3,16 +3,13 @@ package com.school42.swingy;
 import com.school42.swingy.hero.*;
 
 public class Simulator {
-	static double aAttack,  aDefense, aHP;
-	static double bAttack, bDefense, bHP;
 	
 	public static Boolean figth(Hero a, Hero b) {
 		while (a.isAlive() && b.isAlive()) {
-			if (run()) {
-				String damage = a.attack(b);
-			} else {
-				String damage = b.attack(a);
-			}
+			if (run())
+				a.attack(b);
+			else
+				b.attack(a);
 		}
 		return (a.isAlive());
 	}
